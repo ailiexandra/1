1b97a0e8c570a3d07dabd6e9a04dee7bf26548b7
ailiexandra <s17ea2006@gmail.com>
2024-11-27 20:28:14 +0300

    Create задание 3.1Ввести два числа, перевести в двоичное представление.cs
    задание 3.1Ввести два числа, перевести в двоичное представление

---
+++ задание 3.1Ввести два числа, перевести в двоичное представление.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("введите первое число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("введите второе число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        string binary1 = Convert.ToString(num1, 2).PadLeft(8, '0');
+        string binary2 = Convert.ToString(num2, 2).PadLeft(8, '0');
+
+        Console.WriteLine($"первое число в двоичном представлении: {binary1}");
+        Console.WriteLine($"второе число в двоичном представлении: {binary2}");
+
+        int sum = num1 + num2;
+        string binarySum = Convert.ToString(sum, 2).PadLeft(8, '0');
+
+        Console.WriteLine($"сумма в двоичном представлении: {binarySum}");
+
+        Console.WriteLine($"сумма в десятичном виде: {sum}");
+    }
+}
