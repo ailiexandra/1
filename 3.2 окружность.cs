01d25068c680e992d7fcd645a37c744f983206d1
ailiexandra <s17ea2006@gmail.com>
2024-12-11 14:24:03 +0300

    3.2 окружность
    3.2 окружность

---
+++ 3.2 окружность.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int radius = 4;
+        for (int y = 0; y < 2 * radius; y++)
+        {
+            for (int x = 0; x < 2 * radius; x++)
+            {
+                if ((y - radius) * (y - radius) + (x - radius) * (x - radius) < radius * radius)
+                    Console.Write('*');
+                else
+                    Console.Write(' ');
+                    
+            }
+            Console.Write(' ');
+        Console.WriteLine();
+        }
+        
+    }
+}
+
