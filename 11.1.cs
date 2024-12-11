e4d9b2a4c773055558ea09f50b949aeba5677850
ailiexandra <s17ea2006@gmail.com>
2024-12-11 19:50:57 +0300

    11.1 "f.txt"
    11.1 "f.txt"

---
+++ 11.1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+
+    {
+        string basePath = @"C:/Users/me/source/repos";
+        string path = "f.txt";
+        using (StreamWriter sw = new StreamWriter(Path.Combine(basePath, path)))
+        {
+            sw.WriteLine("x\t\tsin(x)");
+            for (double x = 0; x <= 1; x += 0.1)
+            {
+                sw.WriteLine($"{Math.Round(x, 1)}\t\t{Math.Round(Math.Sin(x), 4)}");
+            }
+
+        }
+
+    }
+
+
+}
